eb6c9de80d71288e9b50b91a1bda90c129a44d6e
Eric Northrop <eric.northrop@gmail.com>
2019-04-15 02:27:29 +0000

    Initial Commit

---
+++ index.js
@@ -0,0 +1,30 @@
+// MAIN SERVER FILE
+
+//Main requirements for express and mongo
+const express = require("express");
+const mongojs - require("mongojs");
+
+const app = express();
+
+app.use(express.static("public"));
+
+// DATABASE SETUP
+// variable setup
+let databaseUrl = "news";
+let collections = ["articles"];
+let db = mongojs(databaseUrl, collections);
+// Check for errors
+db.on("error", function(error) {
+  console.log("Database Error:", error);
+});
+
+
+
+// Express Start on port 3000
+app.listen(3000, function() {
+  console.log("App running on port 3000!");
+});
+
+
+
+
