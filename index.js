ac455da220bd9d2017d5aa89374f6b05ce695bf9
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-04-15 20:40:10 -0500

    added axios and cheerio for the time being

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 //Main requirements for express and mongo
 const express = require("express");
 const mongojs = require("mongojs");
+const cheerio = require("cheerio");
+const axios = require("axios");
 
 const app = express();
 
