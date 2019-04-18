596df5dc60b86bfae76360ad60eac250499ad8b5
erictriestocode <43325093+erictriestocode@users.noreply.github.com>
2019-04-17 20:56:51 -0500

    mongoose setup, or at least trying

--- index.js
+++ index.js
@@ -6,20 +6,22 @@ const mongojs = require("mongojs");
 const cheerio = require("cheerio");
 const axios = require("axios");
 const hbs = require("express-handlebars");
+const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.static("public"));
 
 // DATABASE SETUP
+mongoose.connect("mongodb+srv://appuser:appuser@cluster0-8odn4.gcp.mongodb.net/test?retryWrites=true", { useNewUrlParser: true});
 // variable setup
-let databaseUrl = "news";
-let collections = ["articles"];
-let db = mongojs(databaseUrl, collections);
-// Check for errors
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// let databaseUrl = "news";
+// let collections = ["articles"];
+// let db = mongojs(databaseUrl, collections);
+// // Check for errors
+// db.on("error", function(error) {
+//   console.log("Database Error:", error);
+// });
 
 // Main Route
 // sending "server is working" to the DOM as a test
